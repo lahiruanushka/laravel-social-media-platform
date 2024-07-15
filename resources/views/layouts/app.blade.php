543b27b9b277eada0ba2bbe2791dca216538120b
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-15 21:24:00 +0530

    Redesign the UI of app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,16 +15,17 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    <img src="{{asset('icons/celebrate.png')}}" class="me-2" style="width: 40px;">
+                    <img src="{{ asset('icons/celebrate.png') }}" class="me-2" style="width: 40px;">
                     <h2 class="text-header m-0">
-                    PostVibe
-                        
+                        PostVibe
                     </h2>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -34,7 +35,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <!-- Example link -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -77,8 +81,13 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            <div class="container">
+                @yield('content')
+            </div>
         </main>
     </div>
+
+    <!-- Bootstrap JS Bundle with Popper -->
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
