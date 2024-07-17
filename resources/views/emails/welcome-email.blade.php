517a5a0bb08772087f99844ba7c3f5257bb6d63f
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-17 14:56:31 +0530

    Create welcome-email.blade.php

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,13 @@
+<x-mail::message>
+
+# Welcome to PostVibe
+Hello, John
+Postvibe is social media platform.
+
+<x-mail::button :url="''">
+Button Text
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
